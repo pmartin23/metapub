1072a75920d55bf9a7b318b7ca72b95592be0674
nthmost <naomi.most@invitae.com>
2014-11-10 17:59:18 -0800

    working setup.py, PubMedArticle, PubMedFetcher classes

---
+++ metapub/pubmedarticle.py
@@ -0,0 +1,173 @@
+"""medgen.pubmed -- tools to deal with NCBI's E-utilities interface to PubMed"""
+
+# The PubMedArticle class wraps an interface to eutils to fetch articles
+#   and maintains an sqlite cache of previously loaded articles.
+# 
+# The equivalent operation for fetching article with PMID 19483685 would be:
+# curl 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&id=19483685'
+
+import grp, logging, os, pprint, sys
+import xml.etree.ElementTree as ET
+
+logger = logging.getLogger()
+
+class PubMedArticle(object):
+    '''This PubMedArticle class receives an XML string as its required argument
+    and parses it into its constituent parts, exposing them as attributes. 
+
+    Usage:
+        paper = PubMedArticle(xml_string)
+
+    To query services to return an article by pmid, use PubMedFetcher, which
+    returns PubMedArticle objects.
+    '''
+
+    def __init__(self, xmlstr, *args, **kwargs):
+        if not xmlstr:
+            if xmlstr == '':
+                xmlstr = 'empty'
+            raise Exception('Cannot build PubMedArticle object; xml string was %s' % xmlstr)
+        self.article = self._parse_ncbi_xml(xmlstr)
+
+    def _parse_ncbi_xml(self, xmlstr):
+        dom = ET.fromstring(xmlstr)
+        return dom.find('PubmedArticle')
+
+    @property
+    def abstract(self):
+        return( self._get('MedlineCitation/Article/Abstract/AbstractText') )
+
+    @property
+    # N.B. Citations may have 0 authors. e.g., pmid:7550356
+    def authors(self):
+        authors = [ _au_to_last_fm(au) for au in self.article.findall('MedlineCitation/Article/AuthorList/Author') ]
+        return authors
+
+    @property
+    def authors_str(self):
+        return( '; '.join(self.authors) )
+
+    @property
+    def author1_last_fm(self):
+        """return first author's name, in format Last INITS (space between surname and inits)"""
+        return _au_to_last_fm(self.article.find('MedlineCitation/Article/AuthorList/Author'))
+
+    @property
+    def author1_lastfm(self):
+        """return first author's name, in format LastINITS"""
+        if self.author1_last_fm is not None:
+            return self.author1_last_fm.replace(' ','')
+        return None
+
+    @property
+    def journal(self):
+        j = self._get('MedlineCitation/Article/Journal/ISOAbbreviation')
+        if j is None:
+            # e.g., http://www.ncbi.nlm.nih.gov/pubmed?term=21242195
+            j = self._get('MedlineCitation/Article/Journal/Title')
+        assert j is not None
+        return j
+
+    @property
+    def pages(self):
+        return( self._get('MedlineCitation/Article/Pagination/MedlinePgn') )
+
+    @property
+    def first_page(self):
+        try:
+            return self.pages.partition('-')[0]
+        except AttributeError:
+            return None
+
+    @property
+    def title(self):
+        return( self._get('MedlineCitation/Article/ArticleTitle') )
+
+    @property
+    def volume(self):
+        try:
+            return self.article.find('MedlineCitation/Article/Journal/JournalIssue/Volume').text
+        except AttributeError:
+            return None
+
+    @property
+    def issue(self):
+        try:
+            return self.article.find('MedlineCitation/Article/Journal/JournalIssue/Issue').text
+        except AttributeError:
+            return None
+
+    @property
+    def volume_issue(self):
+        ji = self.article.find('MedlineCitation/Article/Journal/JournalIssue')
+        try:
+            return( '%s(%s)' % (ji.find('Volume').text,
+                                ji.find('Issue').text) )
+        except AttributeError:
+            pass
+        try:
+            return( ji.find('Volume').text )
+        except AttributeError:
+            pass
+        # electronic pubs may not have volume or issue
+        # e.g., http://www.ncbi.nlm.nih.gov/pubmed?term=20860988
+        logger.info("No volume for "+self.pmid)
+        return None
+
+    @property
+    def year(self):
+        y = self._get('MedlineCitation/Article/Journal/JournalIssue/PubDate/Year')
+        if y is None:
+            # case applicable for pmid:9887384 (at least)
+            y = self._get('MedlineCitation/Article/Journal/JournalIssue/PubDate/MedlineDate')[0:4]
+        assert y is not None
+        return y
+
+    @property
+    def doi(self):
+        return self._get('PubmedData/ArticleIdList/ArticleId[@IdType="doi"]')
+
+    @property
+    def pii(self):
+        return self._get('PubmedData/ArticleIdList/ArticleId[@IdType="pii"]')
+
+    @property
+    def pmc(self):
+        try:
+            return self._get('PubmedData/ArticleIdList/ArticleId[@IdType="pmc"]')[3:]
+        except TypeError:
+            return None
+
+    ######################################################################
+    ## INTERNAL CLASS FUNCTIONS
+    def _get(self,tag):
+        n = self.article.find(tag)
+        if n is not None:
+            return n.text
+        return None
+    
+    def __str__(self):
+        return( '%s (%s. %s, %s:%s)'.format(
+            self.title, self.authors_str, self.journal, self.volume_issue, self.pages) )
+
+############################################################################
+## Utilities
+
+def _au_to_last_fm(au):
+    if au is None:
+        return
+    try:
+        return( au.find('LastName').text
+                + u' ' + au.find('Initials').text )
+    except AttributeError:
+        pass
+    try:
+        return( au.find('CollectiveName').text )
+    except AttributeError:
+        pass
+    try:
+        return( au.find('LastName').text )
+    except AttributeError:
+        pass
+    raise Exception("Author structure not recognized")
+
---
+++ metapub/pubmedfetcher.py
@@ -0,0 +1,62 @@
+"""medgen.pubmed -- tools to deal with NCBI's E-utilities interface to PubMed"""
+
+import grp, logging, os, pprint, sys
+import xml.etree.ElementTree as ET
+
+logger = logging.getLogger()
+
+from pubmedarticle import PubMedArticle
+
+class Borg:
+  _shared_state = {}
+  def __init__(self):
+    self.__dict__ = self._shared_state
+
+class PubMedFetcher(Borg):
+    '''PubMedFetcher (a Borg singleton object)
+
+    An interaction layer for querying via specified method to return PubMedArticle objects.
+    
+    Currently available methods: eutils
+
+    Basic Usage:
+
+        fetch = PubMedFetcher()
+    
+    To specify a service method (more coming soon):
+
+        fetch = PubMedFetcher('eutils')
+
+    To return an article by querying the service with a known PMID:
+
+        paper = fetch.article_by_pmid('123456')
+
+    Similar methods exist for returning papers by DOI and PM Central id:
+
+        paper = fetch.article_by_doi('10.1186/1750-1172-1-3')
+        paper = fetch.article_by_pmcid('PMC3458974')
+    '''
+
+    def __init__(self, method='eutils'):
+        Borg.__init__(self)
+        self.method = method
+
+        if method=='eutils':
+            import eutils.client as ec
+            self.qs = ec.QueryService()
+            self.article_by_pmid = self._eutils_article_by_pmid
+            self.article_by_pmcid = self._eutils_article_by_pmcid
+            self.article_by_doi = self._eutils_article_by_doi
+
+        else:
+            raise NotImplementedError('coming soon: fetch from local pubmed index.')
+
+    def _eutils_article_by_pmid(self, pmid):
+        return PubMedArticle(self.qs.efetch(args={'db': 'pubmed', 'id': pmid } ))
+
+    def _eutils_article_by_pmcid(self, pmcid):
+        raise NotImplementedError('not yet')
+    
+    def _eutils_article_by_doi(self, doi):
+        raise NotImplementedError('not yet')
+
