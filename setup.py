314c5eb88c76884640e339874ef4710b3c286530
nthmost <naomi.most@invitae.com>
2014-11-30 16:22:59 -0800

    version bump to 0.0.5.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'metapub',
-    version = '0.0.5.4',
+    version = '0.0.5.5',
     description = 'Pubmed / NCBI / eutils interaction library, handling the metadata of pubmed papers.',
     url = 'https://bitbucket.org/nthmost/metapub',
     author = 'Naomi Most',
