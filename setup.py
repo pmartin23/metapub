9957340a5e92f5b8f4651367029f305f89f0b176
nthmost <naomi.most@invitae.com>
2015-06-27 12:52:12 -0700

    v 0.3.14.5 (added vip_nonstandard_journals)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'metapub',
-    version = '0.3.14.4',
+    version = '0.3.14.5',
     description = 'Pubmed / NCBI / eutils interaction library, handling the metadata of pubmed papers.',
     url = 'https://bitbucket.org/metapub/metapub',
     author = 'Naomi Most',
