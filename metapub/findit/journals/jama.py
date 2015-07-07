c6584dfe98880e1619eecb17cc98fe2307ea3bd3
nthmost <naomi.most@invitae.com>
2015-07-07 03:06:57 -0700

    adding the rest of the journals!

---
+++ metapub/findit/journals/jama.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+
+jama_journals = (
+    'Arch Dermatol',
+    'Arch Gen Psychiatry',
+    'Arch Neurol',
+    'Arch Ophthalmol',
+    'Arch Surg',
+    'JAMA',
+    'JAMA Dermatol',
+    'JAMA Facial Plast Surg',
+    'JAMA Intern Med',
+    'JAMA Neurol',
+    'JAMA Oncol',
+    'JAMA Ophthalmol',
+    'JAMA Otolaryngol Head Neck Surg',
+    'JAMA Pediatr',
+    'JAMA Psychiatry',
+    'JAMA Surg',
+)
