718f69393763e3e88575994dd3f3804e80241c6c
nthmost <naomi.most@invitae.com>
2015-08-13 03:33:21 -0700

    pegged eutils version to 0.0.9 until we can get 0.1.0 working.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'metapub',
-    version = '0.3.17.1',
+    version = '0.3.17.2',
     description = 'Pubmed / NCBI / eutils interaction library, handling the metadata of pubmed papers.',
     url = 'https://bitbucket.org/metapub/metapub',
     author = 'Naomi Most',
@@ -17,7 +17,7 @@ setup(
         'setuptools',
         'lxml',
         'requests',
-        'eutils',
+        'eutils==0.0.9',
         'tabulate',
         'cssselect',
         ],
