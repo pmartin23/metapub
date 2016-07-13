a9d6e0905cbcb6c2439bd7bdd4679e3bed262358
Martin Peters <martin@freakybytes.net>
2016-07-13 12:53:20 +0200

    added PubMedAuthor class to __init__ import

--- metapub/__init__.py
+++ metapub/__init__.py
@@ -4,6 +4,7 @@ from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
 
 from .pubmedarticle import PubMedArticle
+from .pubmedauthor import PubMedAuthor
 from .pubmedfetcher import PubMedFetcher
 from .medgenfetcher import MedGenFetcher
 from .medgenconcept import MedGenConcept
